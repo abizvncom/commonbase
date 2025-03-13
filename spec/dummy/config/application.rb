115249ffbd69467d2434abf5adf5c872fa6c3a2f
huan <huanhvhd@gmail.com>
2025-03-13 16:05:00 +0700

    add rspec + update dummy

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -20,10 +20,8 @@ Bundler.require(*Rails.groups)
 
 module Dummy
   class Application < Rails::Application
-    config.load_defaults Rails::VERSION::STRING.to_f
-
-    # For compatibility with applications that use this config
-    config.action_controller.include_all_helpers = false
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 7.2
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
